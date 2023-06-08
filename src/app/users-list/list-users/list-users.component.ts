211754ab320a55abe6d3794341a1c47ef4447c31
Lorenzo Molina Moreno <lmolinamoreno@hotmail.com>
2023-06-08 13:35:01 +0200

    implementada logica de ventanas

--- src/app/users-list/list-users/list-users.component.ts
+++ src/app/users-list/list-users/list-users.component.ts
@@ -1,14 +1,19 @@
 import { Component, isDevMode, OnInit } from '@angular/core';
-import { UsersListService } from "../users-list.service";
-import { GetUserResponse, StateOptions, UpdateUserBody, User } from "../users-list.types";
-import { ConfirmationService, MessageService } from "primeng/api";
-import { DialogService } from "primeng/dynamicdialog";
-import * as _ from 'lodash'
+import { UsersListService } from '../users-list.service';
+import {
+  GetUserResponse,
+  StateOptions,
+  UpdateUserBody,
+  User,
+} from '../users-list.types';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
 })
 export class ListUsersComponent implements OnInit {
   usersList: User[] = [];
@@ -17,39 +22,112 @@ export class ListUsersComponent implements OnInit {
   activeDelete: boolean = false;
   activeEdit: boolean = false;
   activeNewUser: boolean = false;
-  activeStateOptions: StateOptions[] = [{ label: 'Activo', value: true }, { label: 'Inactivo', value: false }]
-  adminStateOptions: StateOptions[] = [{ label: 'Si', value: true }, { label: 'No', value: false }]
+  activeStateOptions: StateOptions[] = [
+    { label: 'Activo', value: true },
+    { label: 'Inactivo', value: false },
+  ];
+  adminStateOptions: StateOptions[] = [
+    { label: 'Si', value: true },
+    { label: 'No', value: false },
+  ];
 
   constructor(
     private listUsersService: UsersListService,
     private confirmationService: ConfirmationService,
-    private dialogService: DialogService,
     private messageService: MessageService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.loading = true;
-    this.listUsersService.getUsersList().subscribe((response: GetUserResponse) => {
-      this.usersList = response.usuarios
-      this.loading = false;
-    })
+    this.listUsersService
+      .getUsersList()
+      .subscribe((response: GetUserResponse) => {
+        this.usersList = response.usuarios;
+        this.loading = false;
+      });
   }
 
-  public confirmSave() {
+  //botones de dialogos de confirmacion//
+  //boton aceptar
+  acceptHandler() {
+    console.log('Accept handler');
+    this.loading = true;
+    if (this.activeDelete) {
+      console.log('delete');
+      this.deleteUser(this.selectedUser.id);
+      return;
+    }
+    if (this.activeEdit) {
+      console.log('edit');
+        this.saveHandler();      
+      //TODO: borrar toast despues de implementar la logica
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Usuario editado correctamente',
+      });
+      return;
+    }
+    if (this.activeNewUser) {
+      console.log('new user');
+      console.log(this.selectedUser);
+      //TODO: borrar toast despues de implementar la logica
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Usuario creado correctamente',
+      });
+      return;
+    }
+  }
+
+  //boton cancelar
+  public cancelHandler() {
+    this.messageService.add({ severity: 'warn', summary: 'Cancelado' });
+    //limpio la variable selectedUser
+    this.selectedUser = new User();
+    this.activeDelete = false;
+    this.activeEdit = false;
+    this.activeNewUser = false;
+  }
+
+  //abrir ventanas de dialogo//
+  //confirmacion al guardar cuando se edita un usuario
+  public confirmEditSave() {
+    this.confirmationService.confirm({
+      message: 'Quieres guardar los cambios de este usuario?',
+      header: 'Confirmar Edicion de usuario',
+      icon: 'pi pi-info-circle',
+      accept: () => this.acceptHandler(),
+      reject: () => this.cancelHandler(),
+    });
     //TODO: dialogo de confirmacion de guardado que llama a esta funcion
   }
 
+  //confirmacion al guardar cuando se crea un nuevo usuario
+  public confirmCreateSave() {
+    this.confirmationService.confirm({
+      message: 'Quieres guardar el nuevo usuario?',
+      header: 'Confirmar nuevo usuario',
+      icon: 'pi pi-info-circle',
+      accept: () => this.acceptHandler(),
+      reject: () => this.cancelHandler(),
+    });
+    //TODO: dialogo de confirmacion de guardado que llama a esta funcion
+  }
+
+  //confirmacion de eliminar usuario
   public confirmDelete(user: User) {
-    this.activeDelete = true;
     this.selectedUser = user;
     this.confirmationService.confirm({
       message: 'Esta seguro de eliminar este usuario?',
       header: 'Confirmar Eliminación',
-      icon: 'pi pi-info-circle'
-    })
+      icon: 'pi pi-info-circle',
+      accept: () => this.deleteUser(user.id),
+      reject: () => this.cancelHandler(),
+    });
   }
 
+  //activar ventanas//
+  //activa la ventana de editar
   public editHandler(user: User) {
     //TODO: Parsear usuarios en el backend
     this.selectedUser = {
@@ -60,47 +138,57 @@ export class ListUsersComponent implements OnInit {
       token: user.token,
       active: user.active == 1,
       admin: user.admin == 1,
-      nick: user.nick
+      nick: user.nick,
     } as User;
-    console.log(this.selectedUser)
-    this.activeEdit = true
+    this.activeEdit = true;
   }
 
-
-
+  //funciones de guardar y borrar//
+  //guardar usuario editado o nuevo
   public saveHandler() {
-    console.log(this.selectedUser);
-    // this.listUsersService.updateUser(data).subscribe((response:Response)=>{
-
-    // })
-    //TODO: Logica para guardar
+    const updateBody:User = this.selectedUser;
+    if (this.activeEdit) {
+      console.log('editando usuario...');
+      this.listUsersService.updateUser(updateBody).subscribe((response:Response)=>{
+          console.log(response)
+          // this.messageService.add({
+          //   severity: 'success',
+          //   summary: 'Usuario editado correctamente',
+          // });
+          this.loading = false;
+      })
+      //TODO: Logica para guardar 
+    }
+    if (this.activeNewUser) {
+      console.log('creando usuario...');
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Usuario creado correctamente',
+      });
+    }
     this.activeEdit = false;
-    this.messageService.add({ severity: 'success', summary: 'Usuario guardado correctamente' });
+    this.activeNewUser = false;
+
     this.selectedUser = new User();
   }
 
-  public deleteHandler(id: number | undefined) {
+  //eliminar usuario
+  public deleteUser(id: number | undefined) {
     this.loading = true;
-    let userId = id || 0
+    let userId = id || 0;
 
-    console.log(userId)
+    console.log(userId);
     //TODO: Logica para eliminar usuario llamando al service
 
     this.loading = false;
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Usuario eliminado correctamente',
+    });
     this.activeDelete = false;
-    this.messageService.add({ severity: 'success', summary: 'Usuario eliminado correctamente' });
-  }
-
-  public cancelHandler() {
-    this.messageService.add({ severity: 'warn', summary: 'Cancelado' });
-    //limpio la variable selectedUser
-    this.selectedUser = new User();
-    this.activeDelete = false;
-    this.activeEdit = false;
-    console.log(this.activeEdit)
   }
 
   public newUserHandler() {
-    console.log('Nuevo usuario...')
+    console.log('Nuevo usuario...');
   }
 }
--- src/app/users-list/users-list.service.ts
+++ src/app/users-list/users-list.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, isDevMode } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from "../environments/environment";
-import { catchError, Observable, tap, throwError } from "rxjs";
-import { GetUserResponse, UpdateUserBody, User } from "./users-list.types";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { GetUserResponse, UpdateUserBody, User } from './users-list.types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersListService {
   private baseUrl: string = '';
@@ -32,21 +32,31 @@ export class UsersListService {
     );
   }
 
-
-  public updateUser(data: UpdateUserBody): Observable<Response> {
-    let updateUserUrl: string = `${this.baseUrl}${environment.usersRoute}`;  
-    return this.http.put<Response>(updateUserUrl, data).pipe(
+  public updateUser(user: User): Observable<Response> {
+    let updateUserUrl: string = `${this.baseUrl}${environment.usersRoute}/${user.id}`;
+    const updateBody = {
+      admin: user.admin,
+      active: user.active
+    }    
+    return this.http.put<Response>(updateUserUrl, updateBody).pipe(
       catchError((e) => {
         if (isDevMode()) {
           console.error(`Error al editar usuario ${e.message}`);
         }
         return throwError(`Error al editar usuario ${e.message}`);
       })
-    )
+    );
   }
-  
 
-  public deleteUser() {
-
+  public deleteUser(id: number) {
+    let deleteUserUrl: string = `${this.baseUrl}${environment.usersRoute}/${id}`;
+    return this.http.delete<Response>(deleteUserUrl).pipe(
+      catchError((e) => {
+        if (isDevMode()) {
+          console.error(`Error al eliminar usuario ${e.message}`);
+        }
+        return throwError(`Error al eliminar usuario ${e.message}`);
+      })
+    );
   }
 }
